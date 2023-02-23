b87919bbf1684d2382a75db881640ba2115f4af3
Owner <sc18ag@brocku.ca>
2023-02-23 11:36:23 -0500

    change the styling a bit

--- museumApp/src/app/unity/unity.component.ts
+++ museumApp/src/app/unity/unity.component.ts
@@ -3,60 +3,20 @@ import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 @Component({
   selector: 'unity',
   templateUrl: './unity.component.html',
-  styleUrls: ['./unity.component.css']
+  styleUrls: ['./unity.component.scss']
 })
-export class UnityComponent implements OnInit, OnDestroy {
-
-  gameInstance: any;
-  progress = 0;
-  isReady = false;
-
-  @Input() version: number = 1;
-
+export class UnityComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    // const loader = (window as any).UnityLoader;
-
-    // this.gameInstance = loader.instantiate('gameContainer', `/assets/Build1/CraigGilchristUnity.json`, {
-    //   onProgress: (gameInstance: any, progress: number) => {
-    //     this.progress = progress;
-    //     if (progress === 1) {
-    //       this.isReady = true;
-    //     }
-    //   }
-    // });
-    var container = document.querySelector("#unity-container");
+    //grabbing the html elements to update the unity loader
+    var container: HTMLElement = document.querySelector("#unity-container");
     var canvas: HTMLElement = document.querySelector("#unity-canvas");
     var loadingBar: HTMLElement = document.querySelector("#unity-loading-bar");
     var progressBarFull: HTMLElement = document.querySelector("#unity-progress-bar-full");
     var fullscreenButton: HTMLElement = document.querySelector("#unity-fullscreen-button");
-    var warningBanner = document.querySelector("#unity-warning");
 
-    // Shows a temporary message banner/ribbon for a few seconds, or
-    // a permanent error message on top of the canvas if type=='error'.
-    // If type=='warning', a yellow highlight color is used.
-    // Modify or remove this function to customize the visually presented
-    // way that non-critical warnings and error messages are presented to the
-    // user.
-    // function unityShowBanner(msg: string, type: string) {
-    //   function updateBannerVisibility() {
-    //     warningBanner.style.display = warningBanner.children.length ? 'block' : 'none';
-    //   }
-    //   var div = document.createElement('div');
-    //   div.innerHTML = msg;
-    //   warningBanner.appendChild(div);
-    //   if (type == 'error') div.style = 'background: red; padding: 10px;';
-    //   else {
-    //     if (type == 'warning') div.style = 'background: yellow; padding: 10px;';
-    //     setTimeout(function () {
-    //       warningBanner.removeChild(div);
-    //       updateBannerVisibility();
-    //     }, 5000);
-    //   }
-    //   updateBannerVisibility();
-    // }
-
+    //grabbing the data built by unity/webgl
     var buildUrl = `/assets/Build`;
     var loaderUrl = buildUrl + "/Room.loader.js";
     var config = {
@@ -67,11 +27,20 @@ export class UnityComponent implements OnInit, OnDestroy {
       companyName: "DefaultCompany",
       productName: "Room",
       productVersion: "0.1",
-      // showBanner: unityShowBanner,
     };
 
-    canvas.style.width = "960px";
-    canvas.style.height = "600px";
+    //find out different sizes
+    if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+      var meta = document.createElement('meta');
+      meta.name = 'viewport';
+      meta.content = 'width=device-width, height=device-height, initial-scale=1.0, user-scalable=no, shrink-to-fit=yes';
+      document.getElementsByTagName('head')[0].appendChild(meta);
+      container.className = "unity-mobile";
+      canvas.className = "unity-mobile";
+    } else {
+      canvas.style.width = "960px";
+      canvas.style.height = "600px";
+    }
 
     loadingBar.style.display = "block";
 
@@ -91,21 +60,4 @@ export class UnityComponent implements OnInit, OnDestroy {
     };
     document.body.appendChild(script);
   }
-
-  ngOnDestroy(): void {
-    this.gameInstance.Quit();
-  }
-
-  // startStopRotating() {
-  //   this.gameInstance.SendMessage('Director', 'StartStopRotating');
-  // }
-
-  // startStopAnimation() {
-  //   this.gameInstance.SendMessage('Director', 'StartStopAnimation');
-  // }
-
-  // setDistance(distance: number) {
-  //   this.gameInstance.SendMessage('Director', 'SetDistance', distance);
-  // }
-
 }

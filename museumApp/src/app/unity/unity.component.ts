3fe2479432a7e8fe217b1a6f3e9b658725aa230b
Owner <sc18ag@brocku.ca>
2023-03-15 12:24:22 -0400

    dont need firebase for unity just send data from UI

--- museumApp/src/app/unity/unity.component.ts
+++ museumApp/src/app/unity/unity.component.ts
@@ -13,70 +13,18 @@ export class UnityComponent implements OnInit {
 
   ngOnInit(): void {
 
-    //grabbing the html elements to update the unity loader
-    var container: HTMLElement = document.querySelector("#unity-container");
-    var canvas: HTMLElement = document.querySelector("#unity-canvas");
-    var loadingBar: HTMLElement = document.querySelector("#unity-loading-bar");
-    var progressBarFull: HTMLElement = document.querySelector("#unity-progress-bar-full");
-    var fullscreenButton: HTMLElement = document.querySelector("#unity-fullscreen-button");
-
-    //grabbing the data built by unity/webgl
+    //grabbing the script to load the unity webgl
     var buildUrl = `/assets/Build`;
-    var loaderUrl = buildUrl + "/Room.loader.js";
-    var config = {
-      dataUrl: buildUrl + "/Room.data",
-      frameworkUrl: buildUrl + "/Room.framework.js",
-      codeUrl: buildUrl + "/Room.wasm",
-      streamingAssetsUrl: "StreamingAssets",
-      companyName: "DefaultCompany",
-      productName: "Room",
-      productVersion: "0.1",
-    };
-
-    //find out different sizes
-    if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
-      var meta = document.createElement('meta');
-      meta.name = 'viewport';
-      meta.content = 'width=device-width, height=device-height, initial-scale=1.0, user-scalable=no, shrink-to-fit=yes';
-      document.getElementsByTagName('head')[0].appendChild(meta);
-      container.className = "unity-mobile";
-      canvas.className = "unity-mobile";
-    } else {
-      //default sizes for desktop
-      canvas.style.width = "960px";
-      canvas.style.height = "600px";
-    }
-
-    //loading bar
-    loadingBar.style.display = "block";
+    var loaderUrl = buildUrl + "/LoadScript.js";
 
     //script that is displaying the build component from webgl
-    var script = document.createElement("script");
-    //instance of an unity webgl
-    var mainUnityInstance;
+    var scriptNew = document.createElement("script");
 
-    //load the script file from webgl
-    script.src = loaderUrl;
-    script.id = 'unity';
+    //load the script file for webgl
+    scriptNew.src = loaderUrl;
 
-    script.onload = () => {
-      this.gameInstance =
-        createUnityInstance(canvas, config, (progress: number) => {
-          progressBarFull.style.width = 100 * progress + "%";
-        }).then((unityInstance: { SetFullscreen: (arg0: number) => void; }) => {
-          loadingBar.style.display = "none";
-          fullscreenButton.onclick = () => {
-            unityInstance.SetFullscreen(1);
-          };
-          //creation of the main instance to do some tweeks
-          mainUnityInstance = unityInstance;
-        }).catch((message: any) => {
-          alert(message);
-        });
-    };
-    //append the loaded script to the html body
-    document.body.appendChild(script);
-
+    //add to body
+    document.body.appendChild(scriptNew);
   }
 
   ngOnDestroy(): void {
---
+++ museumApp/src/assets/Build/LoadScript.js
@@ -0,0 +1,70 @@
+//grabbing the html elements to update the unity loader
+var container = document.querySelector("#unity-container");
+var canvas = document.querySelector("#unity-canvas");
+var loadingBar = document.querySelector("#unity-loading-bar");
+var progressBarFull = document.querySelector("#unity-progress-bar-full");
+var fullscreenButton = document.querySelector("#unity-fullscreen-button");
+
+//grabbing the data built by unity/webgl
+var buildUrl = `/assets/Build`;
+var loaderUrl = buildUrl + "/Room.loader.js";
+var config = {
+    dataUrl: buildUrl + "/Room.data",
+    frameworkUrl: buildUrl + "/Room.framework.js",
+    codeUrl: buildUrl + "/Room.wasm",
+    streamingAssetsUrl: "StreamingAssets",
+    companyName: "DefaultCompany",
+    productName: "Room",
+    productVersion: "0.1",
+};
+
+//find out different sizes
+if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+    var meta = document.createElement('meta');
+    meta.name = 'viewport';
+    meta.content = 'width=device-width, height=device-height, initial-scale=1.0, user-scalable=no, shrink-to-fit=yes';
+    document.getElementsByTagName('head')[0].appendChild(meta);
+    container.className = "unity-mobile";
+    canvas.className = "unity-mobile";
+} else {
+    //default sizes for desktop
+    canvas.style.width = "960px";
+    canvas.style.height = "600px";
+}
+
+//loading bar
+loadingBar.style.display = "block";
+
+//script that is displaying the build component from webgl
+var script = document.createElement("script");
+//instance of an unity webgl
+var mainUnityInstance;
+
+//load the script file from webgl
+script.src = loaderUrl;
+script.id = 'unity';
+
+const data = {
+    name: 'Puffy',
+    description: 'the description',
+    longDescription: 'the long description',
+    imageOne: 'imageOne src',
+    imageTwo: 'imageTwo src',
+    imageThree: 'imageThree src'
+};
+
+script.onload = () => {
+    createUnityInstance(canvas, config, (progress) => {
+        progressBarFull.style.width = 100 * progress + "%";
+    }).then((unityInstance) => {
+        loadingBar.style.display = "none";
+        fullscreenButton.onclick = () => {
+            unityInstance.SetFullscreen(1);
+        };
+        mainUnityInstance = unityInstance
+    }).catch((message) => {
+        alert(message);
+    });
+};
+//append the loaded script to the html body
+document.body.appendChild(script);
